60af94c8aa69352bf24ae82e8242c3bc7a6d546e
gingemonster <brian@earthware.co.uk>
2016-08-04 16:50:21 +0100

    changing example caching period so it actually can be observed by reloading the page

--- Asp.Net.Example/Module.cs
+++ Asp.Net.Example/Module.cs
@@ -11,14 +11,14 @@ namespace Asp.Net.Example
         {
             Get["/"] = _ =>
             {
-                return View["TestView.html", new { Hello = DateTime.Now.ToString(CultureInfo.InvariantCulture)}].AsCacheable(DateTime.Now.AddSeconds(1));
+                return View["TestView.html", new { Hello = DateTime.Now.ToString(CultureInfo.InvariantCulture)}].AsCacheable(DateTime.Now.AddSeconds(10));
             };
 
             Get["/CachedResponse"] = _ =>
             {
                 return Response.AsText(@"
                 this is a cached response: "+DateTime.Now.ToString(CultureInfo.InvariantCulture)+@"
-                ").AsCacheable(DateTime.Now.AddSeconds(1));
+                ").AsCacheable(DateTime.Now.AddSeconds(10));
             };
 
 
